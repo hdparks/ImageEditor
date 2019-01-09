c8933bd95095447698a00d3ff869103682812bb9
Hayden Parkinson <haydendparkinson@gmail.com>
2019-01-09 14:27:20 -0700

    First commit

---
+++ src/ImageEditor.java
@@ -0,0 +1,9 @@
+public class ImageEditor {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        Pixel blue = new Pixel(0,0,255);
+        System.out.println(blue.toString());
+
+
+    }
+}
---
+++ src/Pixel.java
@@ -0,0 +1,12 @@
+public class Pixel {
+    public int r,g,b;
+    Pixel(int r, int g, int b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public String toString(){
+        return this.r + " " + this.g + " " + this.b;
+    }
+}
