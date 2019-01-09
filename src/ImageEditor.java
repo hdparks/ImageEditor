4dabfaa5be6d207f4eb7375eb934b88d3e027290
Hayden Parkinson <haydendparkinson@gmail.com>
2019-01-09 15:40:34 -0700

    Scans in .ppm files

--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -1,9 +1,34 @@
-public class ImageEditor {
-    public static void main(String [] args){
-        System.out.println("Hello World");
-        Pixel blue = new Pixel(0,0,255);
-        System.out.println(blue.toString());
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+public class ImageEditor {
+    public static void main(String[] args) throws IOException {
+        //  Verify edit arguments
+        Path inPath = Paths.get(args[0]);
+
+        //  Read in specified file
+        Scanner scin = new Scanner(inPath, StandardCharsets.UTF_8);
+
+        //  Put everything in a string
+        String fileString = "";
+        while(scin.hasNext()){
+            String n = scin.next();
+            if(n.charAt(0) == '#') {
+                scin.nextLine();
+                continue;
+            }
+
+            fileString += n + " ";
+
+        }
+
+        //  Pass string to PPMImage object
+        //  Call correct edit method
+        //  Call PPMImage.toString
+        //  Write to file
 
     }
 }
--- src/PPMImage.java
+++ src/PPMImage.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class PPMImage {
+
+    int magicNum;
+    int width;
+    int height;
+    int maxVal;
+
+
+    PPMImage(String inString){
+        //  Parse inString, create matrix of Pixels
+    }
 }
