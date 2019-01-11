cdeacba5f70b84ce5fc51a06c8f86a1e1ccc8097
Hayden Parkinson <haydendparkinson@gmail.com>
2019-01-11 14:55:33 -0700

    finished invert, grayscale

--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -1,21 +1,61 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
 
 public class ImageEditor {
 
     public static void main(String[] args) throws IOException {
         //  Validate command line arguments
         if (args.length != 3){
-            //  motion blur takes 4
-            if (args.length != 4){
+            //  Not three arguments
+            if (args.length != 4 || args[2] != "motionblur" || !args[3].matches("-?\\d")){
+                // If there not exactly 4, or
+                // If not "motionblur", or
+                // If not an int for motionblurlength, return Usage statement
+                System.out.println("USAGE: java ImageEditor in-file out-file (grayscale|invert|emboss|motionblur motion-blur-length)");
+                return;
+            }
+
+        } else {
+            // Exactly three values
+
+            //  Third value must be in this list
+            String[] values = {"grayscale","invert","emboss"};
+            boolean contains = Arrays.stream(values).anyMatch(args[2]::equals);
+
+            if (!contains){
+                //   If not one of the list items, return Usage statement
                 System.out.println("USAGE: java ImageEditor in-file out-file (grayscale|invert|emboss|motionblur motion-blur-length)");
                 return;
             }
         }
 
-
+        //  Initialize PPMImage with infile
         PPMImage image = new PPMImage(args[0]);
+
+        switch (args[2]){
+            case "grayscale" : {
+                image.grayscale();
+                break;
+            }
+            case "invert" : {
+                image.invert();
+                break;
+            }
+            case "emboss" : {
+                image.emboss();
+                break;
+            }
+            case "motionblur" : {
+                image.motionblur(Integer.parseInt(args[3]));
+                break;
+            }
+        }
+
         image.exportPPM(args[1]);
+
+        return;
     }
+
 }
--- src/PPMImage.java
+++ src/PPMImage.java
@@ -9,7 +9,7 @@ public class PPMImage {
     int height;
     int maxVal;
 
-    Pixel[] pixels;
+    Pixel[][] pixels;
 
 
     PPMImage(String fileName) throws IOException {
@@ -27,17 +27,23 @@ public class PPMImage {
         this.maxVal = scin.nextInt();
 
         //  Initialize pixel array
-        this.pixels = new Pixel[this.width * this.height];
+        this.pixels = new Pixel[this.height][this.width];
 
         //  Populate pixel array
         int i = 0;
+        int j = 0;
         while(scin.hasNext()){
             int r = scin.nextInt();
             int g = scin.nextInt();
             int b = scin.nextInt();
 
-            this.pixels[i] = new Pixel(r,g,b);
-            i += 1;
+            this.pixels[i][j] = new Pixel(r,g,b);
+            if (j == this.width-1){
+                i++;
+                j = 0;
+            } else {
+                j++;
+            }
         }
     }
 
@@ -60,21 +66,60 @@ public class PPMImage {
         out.newLine();
 
         //  Write pixel values
-        for (Pixel p : this.pixels){
+        for (Pixel[] row : this.pixels){
+            for (Pixel p : row){
+                out.write(p.r+"");
+                out.newLine();
 
-            out.write(p.r+"");
-            out.newLine();
+                out.write(p.g+"");
+                out.newLine();
 
-            out.write(p.g+"");
-            out.newLine();
+                out.write(p.b+"");
+                out.newLine();
+            }
 
-            out.write(p.b+"");
-            out.newLine();
+
         }
         //  Close Writer
         out.close();
     }
 
+    public void grayscale(){
+        for (Pixel[] row : this.pixels){
+            for (Pixel p : row){
+                int avg = (p.r + p.g + p.b) / 3;
+                p.r = p.g = p.b = avg;
+            }
+        }
+    }
 
+    public void invert(){
+        for (Pixel[] row : this.pixels){
+            for (Pixel p : row){
+                p.r = this.maxVal - p.r;
+                p.g = this.maxVal - p.g;
+                p.b = this.maxVal - p.b;
+            }
+        }
+    }
+
+    public void emboss(){
+        //  Work from bottom right across, then up the rows
+        for (int i = this.height - 1; i > 0; i--){
+            for (int j = this.width - 1; j > 0; j--){
+                Pixel p = image[i][j];
+                Pixel o = image[i-1][j-1];
+
+                int rdiff = p.r - o.r;
+                int gdiff = p.g - o.g;
+                int bdiff = p.b - o.b;
+
+            }
+        }
+    }
+
+    public void motionblur(int blurLength){
+
+    }
 
 }
